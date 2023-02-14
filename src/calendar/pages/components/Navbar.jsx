58842b5367602daeadb87ac5be475ded361ff32f
Gerardo Mundo <ger.mundo66@gmail.com>
2023-02-13 19:07:18 -0600

    Fist commit

---
+++ src/calendar/pages/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Navbar = () => {
+  return (
+    <div className='navbar navbar-dark bg-dark mb-4 px-4 p-3 align-items-center'>
+      <span className='navbar-brand'>
+        <i className='fas fa-calendar-alt'></i>
+        &nbsp; Username
+      </span>
+      <button className='btn btn-danger'>
+        <i className='fas fa-sign-out'></i>
+        <span>Salir</span>
+      </button>
+    </div>
+  );
+};
