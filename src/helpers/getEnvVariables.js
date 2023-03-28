a9800fdb9fa17de656bc975ed8984ebfcc13f92d
Gerardo Mundo <ger.mundo66@gmail.com>
2023-03-27 22:25:59 -0600

    Build de prod

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -1,6 +1,15 @@
 export const getEnvVariables = () => {
-  import.meta.env;
+  import.meta?.env
   return {
     ...import.meta.env,
-  };
-};
+  }
+}
+
+// export const getEnvVariables = () => {
+//   console.log('process', import.meta.env)
+//   if (typeof process !== 'undefined') {
+//     return { ...process.env }
+//   } else {
+//     return { ...import.meta.env }
+//   }
+// }
